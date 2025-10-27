764fa94f45728ee762ff65cd07e76073dc0abfe2
Rakib Hossain <rakib@example.com>
2025-10-27 19:44:52 +0600

    added some apis

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7ks5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -24,12 +23,25 @@ async function run() {
     try {
         await client.connect();
 
-            const itemCollection = client.db("naturo_db").collection('items');
+        const itemCollection = client.db("naturo_db").collection('items');
 
+        // items related apis 
         app.get('/items', async (req, res) => {
-            const result = await itemCollection.find().toArray();
-            res.send(result);
-        })
+            try {
+                const search = req.query.search || "";
+                let query = {};
+                if (search.trim() !== "") {
+                    query = {
+                        name: { $regex: search, $options: "i" }
+                    };
+                }
+                const result = await itemCollection.find(query).toArray();
+                res.send(result);
+            } catch (error) {
+                console.error("Error fetching items:", error);
+                res.status(500).send({ message: "Internal Server Error" });
+            }
+        });
 
         app.get('/items/:id', async (req, res) => {
             const id = req.params.id;
@@ -38,6 +50,35 @@ async function run() {
             if (!result) {
                 return res.status(404).send({ message: "Item not found" });
             }
+            res.send(result);
+        })
+
+        app.delete('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.patch('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const product = req.body;
+            const query = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    name: product.name,
+                    category: product.category,
+                    image: product.image,
+                    presentPrice: product.presentPrice
+                }
+            }
+            const result = await itemCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+        app.post('/items', async (req, res) => {
+            const product = req.body;
+            const result = await itemCollection.insertOne(product);
             res.send(result);
         })
 
